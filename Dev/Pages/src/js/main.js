f833ca644f1f1ed91ab3101d8a87014aee4c58de
Eugen <eugen.haidukov@netzlabor-coding.de>
2020-03-16 11:08:38 +0200

    first commit

---
+++ Dev/Pages/src/js/main.js
@@ -0,0 +1,34 @@
+import $ from "jquery";
+
+import "bootstrap";
+
+window.$ = window.jQuery = $;
+
+// import accordion from "./components/accordion";
+
+
+
+// $(() => {
+//   polyfill.init();
+//   preloader.init();
+//   accordion.init();
+//   hamburgers.init();
+//   footerList.init();
+//   hero.init();
+//   weather.init();
+//   navigation.init();
+//   teaserEventSwiper.init();
+//   animate.init();
+//   header.init();
+//   automplete.init();
+//   nlTabs.init();
+//   textfield.init();
+//   gallery.init();
+//   calendar.init();
+//   zoomImage.init();
+//   mainmenuPopover.init();
+//   language.init();
+//   metaSearch.init();
+//   weatherStories.init();
+//   teaserSwiper.init();
+// });
